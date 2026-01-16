fbac99b8d759fde7d2707fae0c8b040fe874a008
Memoona-754 <memoonamehboob754@gmail.com>
2026-01-16 18:32:20 +0500

    Add implementation of Student class with example usage

---
+++ OOPs/userdefineddatatype.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Student{
+    public:
+          string name;
+          int rollno;
+          float gpa;
+};
+int main(){
+    Student s1;
+    s1.name = "Memoona";
+    s1.rollno = 166;
+    s1.gpa = 8.2;
+
+    Student s2;
+    s2.name = "Marium";
+    s2.rollno = 168;
+    s2.gpa = 8.9;
+
+    cout<<s1.name<<" "<<s1.rollno<<" "<<s1.gpa<<" "<<endl;
+    cout<<s2.name<<" "<<s2.rollno<<" "<<s2.gpa<<" "<<endl;
+}
