98334bd5251faba1467121ac90cc6cfcb33b8171
Memoona-754 <memoonamehboob754@gmail.com>
2026-01-29 21:01:30 +0500

    C++

---
+++ C++/String/lidraries.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    string name = "Memoona";
+    cout<<"The name is "<<name<<endl;
+    cout<<"The length of name is :"<<name.length()<<endl;
+    cout<<"The name is : "<<name.substr(1,155)<<endl;
+    cout<<"The name is :"<<name.sbstr(2,3);
+
+    return 0;
+}
