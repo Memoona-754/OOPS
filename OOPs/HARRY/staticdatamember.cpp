d4978b5d68df14daff20f48a1e00e5c466be6302
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-02 23:08:41 +0500

    C++

--- OOPs/HARRY/staticdatamember.cpp
+++ OOPs/HARRY/staticdatamember.cpp
@@ -6,14 +6,21 @@ class Employee{
     int count;
     public:
     void setData(void){
+        cout<<"Entrer the id "<<endl;
         cin>>id;
     }
     void getData(void){
-        cout<<id;
+        cout<<"The id of this employee is "<<id;
     }
 };
 
 int main(){
-    Emplyee harry;
-    harry.id;
+    Employee harry;
+   /* harry.id=1;
+    harry.count = 1;  con't do this bcz id and count are private*/
+
+    harry.setData();
+    harry.getData();
+
+    return 0;
 }
