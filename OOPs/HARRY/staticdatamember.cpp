f04ac28c8a84be7d2123e88a8c1d06c837dc311e
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-02 19:00:20 +0500

    C++

---
+++ OOPs/HARRY/staticdatamember.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+uisng namespace std;
+
+class Employee{
+    int id;
+    int count;
+    public:
+    void setData(void){
+        cin>>id;
+    }
+    void getData(void){
+        cout<<id;
+    }
+};
+
+int main(){
+    Emplyee harry;
+    harry.id;
+}
