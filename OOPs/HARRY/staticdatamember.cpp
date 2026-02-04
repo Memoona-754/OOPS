07440292f5b5722d797e1c41e84c2c5f36303015
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-04 21:44:52 +0500

    C++

--- OOPs/HARRY/staticdatamember.cpp
+++ OOPs/HARRY/staticdatamember.cpp
@@ -3,24 +3,37 @@ uisng namespace std;
 
 class Employee{
     int id;
-    int count;
+    static int count; //value of static variable is defaultly 0(zero) 
     public:
     void setData(void){
-        cout<<"Entrer the id "<<endl;
+        cout<<"Entr the id "<<endl;
         cin>>id;
+        count++;
     }
     void getData(void){
-        cout<<"The id of this employee is "<<id;
+        cout<<"The id of this employee is "<<id<<"and this is employee number "<<count<<endl;
+    }
+    static void getCount(void){
+        cout<<"The value of count is "<<count<<endl;
     }
 };
 
+int  Employee :: count;
+
 int main(){
-    Employee harry;
+    Employee ali,ahad,ahmad;
    /* harry.id=1;
     harry.count = 1;  con't do this bcz id and count are private*/
 
-    harry.setData();
-    harry.getData();
-
+    ali.setData();
+    ali.getData();
+    Employee::getCount();  
+    ahad.setData();
+    ahad.getData();
+    Employee::getCount();
+    ahmad.setData();
+    ahmad.getData();
+    Employee::getCount();
+    
     return 0;
 }
