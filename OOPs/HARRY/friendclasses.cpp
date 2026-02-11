3484b342134bdc8a4dc06f70c4304b0d733c46ca
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-11 21:15:51 +0500

    C++

--- OOPs/HARRY/friendclasses.cpp
+++ OOPs/HARRY/friendclasses.cpp
@@ -10,11 +10,14 @@
         return(a+b);
     }
     int sumRealComplex(Complex,Complex);
+    int sunCompCopmlex(Complex,Complex);
  };
 
  class Complex{
     int a,b;
-    friend int Calculator :: sumRealComplex(Complex o1, Complex o2); 
+    //Individually declaring functions as friends
+    friend int Calculator :: sumRealComplex(Complex , Complex );
+    friend int Calculator :: sumCompComplex(Complex ,Complex); 
     public:
      void setnumber (int n1, int n2){
         a = n1;
@@ -28,6 +31,9 @@
  int Calculator :: sumRealComplex(Complex o1,Complex o2){
         return (o1.a + o2.a); 
     }
+ int Calculator :: sumCompComplex(Complex o1,Complex o2){
+        return (o1.b + o2.b); 
+    }    
 
  int main(){
     Complex o1, o2;
@@ -36,5 +42,8 @@
     Calculator calc;
     int res = calc.sumRealComplex(o1,o2);
     cout<<"The sum of real partof o1 and o2 is "<<res<<endl;
+    int resc = calc.sunCompCopmlex (o1,o2);
+    cout<<"The sum of complex part of o1 and o2 is "<<resc<<endl;
+
     return 0; 
  }
