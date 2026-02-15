427127270b199ee74fea9850e2bfa25b8b158bc2
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-15 16:53:44 +0500

    C++

---
+++ OOPs/HARRY/constructoroverloading.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Complex{
+    int a,b;
+    public:
+    Complex(int x, int y){
+        a = x;
+        b = y;
+    }
+    void printNumber(){
+        cout<<"Yoyr number is "<<a<<" + "<<b <<" i "<<endl;
+    }
+};
+int main(){
+    Complex c1(4,6);
+    c1.printNumber();
+    return 0;
+}
