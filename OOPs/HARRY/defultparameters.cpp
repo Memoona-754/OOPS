5b39c595b51cd96f82bc83ed004ff8552cec5421
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-16 00:43:02 +0500

    C++

--- OOPs/HARRY/defultparameters.cpp
+++ OOPs/HARRY/defultparameters.cpp
@@ -4,14 +4,14 @@ class Simple{
     int data1;
     int data2;
     public:
-    Simple(int a, int b=9){
+    Simple(int a, int b=9){   //here b has defult value
        data1 = a;
        data2=b;
     }
     void printData();
 };
 void Simple :: printData(){
-    cout<<"The value of data1 and data2 is "<<data1<<" and "<<data2<<endl;
+    cout<<"The value of data1 and data2  is "<<data1<<" and "<<data2<<endl;
 } 
 int main(){
     Simple s(1);
