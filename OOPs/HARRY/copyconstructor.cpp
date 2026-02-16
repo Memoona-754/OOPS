c9673c87d79606760b07fa964616c7469d2831d1
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-17 00:25:18 +0500

    C++

--- OOPs/HARRY/copyconstructor.cpp
+++ OOPs/HARRY/copyconstructor.cpp
@@ -1,16 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Number{
+class Number
+{
     int a;
-    public:
-    Number(int num){
-        a=num;
+
+public:
+    Number()
+    {
+        a = 0;
     }
-    void display(){
-        cout<<"The number for this object is "<<a<<endl;
+    Number(int num)
+    {
+        a = num;
+    }
+    // when no copy constructor is found, compiler supplies its own copy constructor thats way this code is working even after commiting below copy constructor
+    //  Number(Number &obj){
+    //      cout<<"Copy constructor called!!!"<<endl;
+    //      a=obj.a;
+    //  }
+
+    void display()
+    {
+        cout << "The number for this object is " << a << endl;
     }
 };
-int main(){
-    Number x,y,z;
+int main()
+{
+    Number x, y, z(45),z2;
+    x.display();
+    y.display();
+    z.display();
+    Number z1(z); // copy constructor invoked
+    z1.display();
+    z2 = z;  
+    z2.display();      // copy constructor not called
+    Number z3 = z; // copy costructor invoked
+    z3.display();
     return 0;
 }
