151cbde676857b5dc29e7e2acd2f5bd6652afdb7
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-23 23:27:30 +0500

    C++

--- OOPs/HARRY/Inheritancedeep.cpp
+++ OOPs/HARRY/Inheritancedeep.cpp
@@ -1,41 +1,53 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Base{
+class Base
+{
     int data1;
-    public:
-    int setData;
+
+public:
+    int data2;
+    int setData();
     int getData1();
     int getData2();
-
 };
-void Base :: setData(void){
+void Base ::setData(void)
+{
     data1 = 10;
     data2 = 20;
 }
-int Base :: getData1(){
+int Base ::getData1()
+{
     return data1;
 }
-int Base :: getData2(){
-    return data2;   
+int Base ::getData2()
+{
+    return data2;
 }
-class Derived : public Base{
+class Derived : public Base
+{
     int data3;
-    public:
+
+public:
     void process();
     void display();
 };
-void Derived :: process(){
+void Derived ::process()
+{
     data3 = data2 * getData1();
 }
-void Derived :: display(){
-    cout<<"Value of data 1 is "<< getData1()<<endl;
-    cout<<"Value of data2 is "<<data2<<endl;
-    cout<<"Value of data3 is  "<<data3<<endl;  
+void Derived ::display()
+{
+    cout << "Value of data 1 is " << getData1() << endl;
+    cout << "Value of data2 is " << data2 << endl;
+    cout << "Value of data3 is  " << data3 << endl;
 }
 
-int main(){
-    Derived der;  
+int main()
+{
+    Derived der;
     der.setData();
     der.process();
     der.display();
+
+    return 0;
 }
