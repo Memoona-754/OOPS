4f108c0cdeb1ae9212e7a817154ccae7b9ecd3ca
Memoona-754 <memoonamehboob754@gmail.com>
2026-02-27 02:48:37 +0500

    C++

--- OOPs/HARRY/multipleinheritance.cpp
+++ OOPs/HARRY/multipleinheritance.cpp
@@ -24,12 +24,16 @@ class Base2{
 class Derived : public  Base1,public Base2{
     public:
     void show(){
-        cout<<"The value of base1 is"<<base1int<<endl;
-        cout<<"The value of base2 is"<<base2int<<endl;
-        cout<<"The sum of values is"<<base1int  + base2int<<endl;
+        cout<<"The value of base1 is "<<base1int<<endl;
+        cout<<"The value of base2 is "<<base2int<<endl;
+        cout<<"The sum of values is "<<base1int  + base2int<<endl;
 
     }
 };
 int main(){
+    Derived memoona;
+    memoona.set_base1int(44);
+    memoona.set_base2int(33);
+    memoona.show();
     return 0;
 }
