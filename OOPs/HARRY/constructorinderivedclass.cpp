01c622d2ed58d25186098098e9d1a6ec0ed33106
Memoona-754 <memoonamehboob754@gmail.com>
2026-03-18 00:56:38 +0500

    C++

--- OOPs/HARRY/constructorinderivedclass.cpp
+++ OOPs/HARRY/constructorinderivedclass.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 class Base1{
     int data1;
-    public:
+    public:  
     Base1 (int i){
         data1=i;
         cout<<"Base1 class constructor called "<<endl;
     }
-    void printData(void){
+    void printDatabase1(void){
         cout<<"The value of data1 is "<<data1<<endl;
     }
 };
@@ -18,7 +18,7 @@ class Base2{
         data2=i;
         cout<<"Base2 class constructor called "<<endl;
     }
-    void printData(void){
+    void printDatabase2(void){
         cout<<"The value of data2 is "<<data2<<endl;
     }
 };
@@ -29,10 +29,11 @@ class Derived: public Base1, public Base2{
         derived1=c;
         derived2=d;
     }
-    void printData(void){
-        cout<<"The value of data2 is "<<data2<<endl;
+    void printDataderived(void){
+        cout<<"The value of  derived1 is "<< derived1<<endl;
+        cout<<"The value of derived2 is "<< derived2<<endl;
     }
 };
 int main(){
      
-}
+}  
