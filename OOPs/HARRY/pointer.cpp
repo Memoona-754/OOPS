c501138b340ff7a9d0044dae2b143cf3f52b3767
Memoona-754 <memoonamehboob754@gmail.com>
2026-03-27 15:44:15 +0500

    C++

--- OOPs/HARRY/pointer.cpp
+++ OOPs/HARRY/pointer.cpp
@@ -10,6 +10,14 @@ int main(){
     int *p= new int(30);
     cout<<"The value at address p is "<<*(p)<<endl;
     float *q= new float(30.9999);
-    cout<<"The value at address q is "<<*(q);
+    cout<<"The value at address q is "<<*(q)<<endl;
+
+    int *arr = new int[3];
+    arr[0]=10;
+    arr[1]=20;
+    arr[2]=30;
+    cout<<"The value of arr[0] is "<<arr[0]<<endl;
+     cout<<"The value of arr[1] is "<<arr[1]<<endl;
+      cout<<"The value of arr[2] is "<<arr[2]<<endl;
     return 0; 
 }
