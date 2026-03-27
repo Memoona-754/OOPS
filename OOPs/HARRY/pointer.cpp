36f07242b569d8065fd3c8dd019837d5f032b9e5
Memoona-754 <memoonamehboob754@gmail.com>
2026-03-27 15:36:02 +0500

    C++

--- OOPs/HARRY/pointer.cpp
+++ OOPs/HARRY/pointer.cpp
@@ -5,5 +5,9 @@ int main(){
     int a=4;
     int *ptr = &a;
     cout<<"The value of a is "<<*(ptr)<<endl;
+
+    //new keyword
+    int *p= new int(30);
+    cout<<"The value at address p is "<<*(p);
     return 0; 
 }
