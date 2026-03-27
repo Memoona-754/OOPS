7fb3c45e6741d4de4c1520b714e5448dfe7ef1b6
Memoona-754 <memoonamehboob754@gmail.com>
2026-03-27 15:57:58 +0500

    C++

--- OOPs/HARRY/pointer.cpp
+++ OOPs/HARRY/pointer.cpp
@@ -12,12 +12,21 @@ int main(){
     float *q= new float(30.9999);
     cout<<"The value at address q is "<<*(q)<<endl;
 
+    // int *arr = new int[3];
+    // arr[0]=10;
+    // arr[1]=20;
+    // arr[2]=30;
+    // cout<<"The value of arr[0] is "<<arr[0]<<endl;
+    // cout<<"The value of arr[1] is "<<arr[1]<<endl;
+    // cout<<"The value of arr[2] is "<<arr[2]<<endl;
+
     int *arr = new int[3];
     arr[0]=10;
     arr[1]=20;
     arr[2]=30;
+    delete[] arr;
     cout<<"The value of arr[0] is "<<arr[0]<<endl;
-     cout<<"The value of arr[1] is "<<arr[1]<<endl;
-      cout<<"The value of arr[2] is "<<arr[2]<<endl;
+    cout<<"The value of arr[1] is "<<arr[1]<<endl;
+    cout<<"The value of arr[2] is "<<arr[2]<<endl;
     return 0; 
 }
--- OOPs/HARRY/tempCodeRunnerFile.cpp
+++ OOPs/HARRY/tempCodeRunnerFile.cpp
@@ -1,4 +1,8 @@
- // Number(Number &obj){
-    //     cout<<"Copy constructor called!!!"<<endl;
-    //     a=obj.a;
-    // }
+
+    // int *arr = new int[3];
+    // arr[0]=10;
+    // arr[1]=20;
+    // arr[2]=30;
+    // cout<<"The value of arr[0] is "<<arr[0]<<endl;
+    // cout<<"The value of arr[1] is "<<arr[1]<<endl;
+    // cout<<"The value of arr[2] is "<<arr[2]<<endl;
