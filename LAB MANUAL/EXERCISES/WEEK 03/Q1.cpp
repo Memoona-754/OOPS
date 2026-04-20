d210e6c5e7b2943a6a4d47d3da0732261a4c26bb
Memoona-754 <memoonamehboob754@gmail.com>
2026-04-20 18:21:16 +0500

    C++

--- LAB MANUAL/EXERCISES/WEEK 03/Q1.cpp
+++ LAB MANUAL/EXERCISES/WEEK 03/Q1.cpp
@@ -1,55 +1,53 @@
 #include <iostream>
 using namespace std;
 
-class Complex
-{
+class Complex {
 private:
-    int *real;
-    int *imag;
+    double* real;
+    double* imag;
 
 public:
     // Constructor
-    Complex(int r, int i)
-    {
-        real = new int;
-        imag = new int;
-        *real = r;
-        *imag = i;
-    }
+  Complex(double r, double i) {
+    real = new double(r);    // allocate a double on heap, initialise to r
+    imag = new double(i);    // allocate a double on heap, initialise to i
+}
 
     // Copy Constructor (Deep Copy)
-    Complex(const Complex &obj)
-    {
-        real = new int;
-        imag = new int;
-        *real = *(obj.real);
-        *imag = *(obj.imag);
+    Complex(const Complex& obj) {
+        real = new double(*(obj.real));
+        imag = new double(*(obj.imag));
     }
 
-    // Display function
-    void display()
-    {
-        cout << "Complex Number: " << *real << " + " << *imag << "i" << endl;
+    // Copy Assignment Operator (Rule of Three)
+    Complex& operator=(const Complex& obj) {
+        if (this != &obj) {         // self-assignment guard
+            *real = *(obj.real);    // reuse existing memory
+            *imag = *(obj.imag);
+        }
+        return *this;
+    }
+
+    void display() const {
+        cout << *real << " + " << *imag << "i\n";
     }
 
     // Destructor
-    ~Complex()
-    {
-        delete real;
-        delete imag;
+    ~Complex() {
+        delete real;  real = nullptr;
+        delete imag;  imag = nullptr;
     }
 };
 
-int main()
-{
-    Complex c1(3, 4); // Original object
-    Complex c2 = c1;  // Copy constructor called
+int main() {
+    Complex c1(3.5, 4.2);
+    Complex c2 = c1;          // copy constructor
+    Complex c3(0, 0);
+    c3 = c1;                  // copy assignment operator
 
-    cout << "Object 1: ";
-    c1.display();
-
-    cout << "Object 2 (Copied): ";
-    c2.display();
+    cout << "c1 (original): "; c1.display();
+    cout << "c2 (copied):   "; c2.display();
+    cout << "c3 (assigned): "; c3.display();
 
     return 0;
 }
