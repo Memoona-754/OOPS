457da5298818b3f7a011df5aad33b7ce394a7282
Memoona-754 <memoonamehboob754@gmail.com>
2026-05-05 16:37:57 +0500

    C++

--- OOPs/m3.cpp
+++ OOPs/m3.cpp
@@ -1,35 +1,85 @@
+// #include<iostream>
+// using namespace std;
+// class Employee{
+//     public:
+//     string name;
+//     virtual void work(){
+//         cout<<"Employee is working"<<endl;
+//     }
+// };
+// class Developer:public Employee{
+//     public:
+//     void work()override{
+//         cout<<"Developer is writing code"<<endl;
+//     }
+// };
+// class Designer : public Employee{
+//     public:
+//     void work()override{
+//         cout<<"Designer is making UI"<<endl;
+//     }
+// };
+// int main(){
+//     Employee* a = new Employee();
+//     Employee* b = new Developer();
+//     Employee* c= new Designer();
+
+//     a->work();
+//     b->work();
+//     c->work();
+
+//     delete a;
+//     delete b;
+//     delete c;
+
+// }
 #include<iostream>
 using namespace std;
-class Employee{
+class Account{
     public:
-    string name;
-    virtual void work(){
-        cout<<"Employee is working"<<endl;
+    string ownername;
+    Account(){
+        cout<<"Account created\n";
+    }
+    virtual void showtype(){
+        cout<<"Basic Account\n";
+    }
+    virtual ~  Account()
+    {
+        cout<<"Account closed\n";
     }
 };
-class Developer:public Employee{
+class Savingaccount : public Account{
     public:
-    void work()override{
-        cout<<"Developer is writing code"<<endl;
+    void showtype()override{
+        cout<<"Saving account\n";
+    }
+    ~ Savingaccount(){
+        cout<<"Saving accout closed\n";
     }
 };
-class Designer : public Employee{
+class Currentaccount : public Account{
     public:
-    void work()override{
-        cout<<"Designer is making UI"<<endl;
+    void showtype ( ) override{
+        cout<<"Current account\n";
+    }
+    ~Currentaccount(){
+        cout<<"Current account closed\n";
     }
 };
+
 int main(){
-    Employee* a = new Employee();
-    Employee* b = new Developer();
-    Employee* c= new Designer();
+    Account* arr[3];
+    arr[0]= new Account();
+    arr[1]=new Savingaccount();
+    arr[2]=new Currentaccount();
 
-    a->work();
-    b->work();
-    c->work();
+    for(int i=0;i <3;i++){
+        arr[i]->showtype();
+    }
+    for (int i=0;i<3;i++){
+        delete arr[i];
+    }
 
-    delete a;
-    delete b;
-    delete c;
-
+    return 0;
 }
